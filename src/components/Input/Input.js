d19e2ee3ef8f65eeb1fc4f5be9de17e3838b6042
Jean Ferri <jeanferrimh@gmail.com>
2024-01-27 21:05:51 -0300

    criado state de livros pesquisados

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 const Input = styled.input`
     padding: 10px 10px;
-    border: 1px solid white;
+    border: 1px solid black;
     border-radius: 20px;
     text-align: center;
     width: 100%;
+    
 }
 `
 
--- src/components/Pesquisa/Pesquisa.js
+++ src/components/Pesquisa/Pesquisa.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Input from "../Input/Input";
+import { useState } from "react";
+import { livros } from "./livrosPesquisa";
 
 const PesquisaContainer = styled.section`
 display:flex;
@@ -11,7 +13,7 @@ margin: 30px 25%
 `
 
 const Titulo = styled.h2`
-    font-size:36px;
+    font-size:38px;
     font-weigth: bold;
     text-align:center;
     color: white;
@@ -22,12 +24,44 @@ const SubTitulo = styled.h3`
     color: white;
 `
 
+const LivrosPesquisadosContainer = styled.section`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    width:90%;
+`
+
+const livroContainer = styled.div`
+    width: 25%;
+    padding:20px;
+`
+
 function Pesquisa(){
+
+    const [LivrosPesquisa, setLivrosPesquisa] = useState([])
+
+    function fazPesquisa(valor){
+        const texto = valor.target.value
+                    const livrosPesquisados = livros.filter(livro => livro.nome.includes(texto))
+                    setLivrosPesquisa(livrosPesquisados)
+    }
+
     return(
         <PesquisaContainer>
-            <Titulo>Sabe por onde começar ?</Titulo>
-            <SubTitulo>Procure o seu livro perfeito</SubTitulo>
-            <Input placeholder="Procure por algum livro"></Input>
+            <Titulo>Já sabe por onde começar ?</Titulo>
+            <SubTitulo>Procure o seu livro em nossa estante</SubTitulo>
+            <Input placeholder="Procure por algum livro"
+                onBlur={evento => fazPesquisa(evento)}
+            ></Input>
+        <LivrosPesquisadosContainer>
+        {LivrosPesquisa.map(livro => (
+                <livroContainer>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src}></img>
+                </livroContainer>
+        )
+            )}
+        </LivrosPesquisadosContainer>
         </PesquisaContainer>
     )
 }
